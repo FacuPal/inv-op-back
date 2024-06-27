723df97ab8ba7d8d3294bd481e83b9434c3c7333
LuccaB13 <Lucca.13@outlook.com>
2024-06-27 15:09:16 -0300

    Cantidad Vendida incorporada

--- src/main/java/com/inv/op/backend/dto/DTOProductoLista.java
+++ src/main/java/com/inv/op/backend/dto/DTOProductoLista.java
@@ -1,35 +1,34 @@
 package com.inv.op.backend.dto;
 
 import java.io.Serializable;
-
 import com.inv.op.backend.model.InventoryModel;
 import com.inv.op.backend.model.Product;
-
 import com.inv.op.backend.model.ProductFamily;
+import com.inv.op.backend.model.HistoricDemand;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class DTOProductoLista implements Serializable {
-    public DTOProductoLista(Product product) {
+    public DTOProductoLista(Product product, List<HistoricDemand> historicDemands) {
         this.productId = product.getProductId();
         this.productName = product.getProductName();
         this.productDescription = product.getProductDescription();
         this.productFamilyName = product.getProductFamily().getProductFamilyName();
         this.inventoryModelName = product.getProductFamily().getInventoryModel().getInventoryModelName();
         this.stock = product.getStock();
-
-        this.storageCost=product.getStorageCost();
-        this.orderCost=product.getOrderCost();
-        this.unitCost= product.getUnitCost();
-
-        this.isDeleted=product.getIsDeleted();
-        this.productFamilyId=product.getProductFamily().getProductFamilyId();
-        this.productDemand=product.getProductDemand();
-        this.maxStock=product.getMaxStock();
+        this.storageCost = product.getStorageCost();
+        this.orderCost = product.getOrderCost();
+        this.unitCost = product.getUnitCost();
+        this.isDeleted = product.getIsDeleted();
+        this.productFamilyId = product.getProductFamily().getProductFamilyId();
+        this.productDemand = product.getProductDemand();
+        this.maxStock = product.getMaxStock();
+        this.totalHistoricDemand = historicDemands.stream().mapToInt(HistoricDemand::getQuantity).sum();
     }
 
     private Long productId;
@@ -45,4 +44,5 @@ public class DTOProductoLista implements Serializable {
     private Long productFamilyId;
     private Integer maxStock;
     private Integer productDemand;
+    private Integer totalHistoricDemand; // Nuevo atributo
 }
